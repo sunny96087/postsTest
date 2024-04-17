642ab8f5de9bf62e23ec27814a11d2b97dfc04d5
sunny96087 <sunny96087@yahoo.com.tw>
2024-04-17 21:29:35 +0800

    整理 code

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const http = require('http');
 const mongoose = require('mongoose');
 const headers = require('./headers');
-const dotenv = require("dotenv");
 const handleError = require('./handleError');
 const handleSuccess = require('./handleSuccess');
 
-
+const dotenv = require("dotenv");
 // 導入上面的 .env 檔
 dotenv.config({ path: "./config.env" });
 
@@ -35,8 +34,7 @@ const requestListener = async(req, res)=>{
 
     if(req.url=="/posts" && req.method == "GET"){
         const post = await Post.find();
-        handleSuccess(res, post);
-        // res.end();
+        handleSuccess(res, post, '取得所有資料成功');
     }else if(req.url=="/posts" && req.method == "POST"){
         req.on('end',async()=>{
             try{
@@ -48,12 +46,6 @@ const requestListener = async(req, res)=>{
                             content: data.content,
                         }
                     );
-                    // res.writeHead(200,headers);
-                    // res.write(JSON.stringify({
-                    //     "status": "success",
-                    //     "data": newPost,
-                    // }));
-                    // res.end();
                     handleSuccess(res, newPost, '新增單筆資料成功');
                 }else{
                   handleError(res);
@@ -65,26 +57,10 @@ const requestListener = async(req, res)=>{
     }else if (req.url == "/posts" && req.method == "DELETE") {
         const data = await Post.deleteMany({}); // {} 刪除全部
         handleSuccess(res, [], '刪除全部資料成功');
-        // res.writeHead(200, headers);
-        // res.write(
-        //   JSON.stringify({
-        //     "status": "success",
-        //     "data": [],
-        //     "message": "刪除全部資料成功"
-        //   })
-        // );
-        // res.end();
     }else if(req.url.startsWith("/posts/") && req.method=="DELETE"){
         const id = req.url.split('/').pop();
         await Post.findByIdAndDelete(id);
         handleSuccess(res, null, '刪除單筆資料成功');
-        // res.writeHead(200,headers);
-        // res.write(JSON.stringify({
-        //     "status": "success",
-        //     "data": null,
-        //     "message": "刪除單筆資料成功"
-        // }));
-        // res.end();
     }else if (req.url.startsWith("/posts/") && req.method == "PATCH") {
         req.on("end", async () => {
           try {
@@ -102,37 +78,11 @@ const requestListener = async(req, res)=>{
     
             if (updatedPost) {
               handleSuccess(res, updatedPost, '更新單筆資料成功');
-              // res.writeHead(200, headers);
-              // res.write(
-              //   JSON.stringify({
-              //     "status": "success",
-              //     "data": updatedPost,
-              //     "message": "更新單筆資料成功",
-              //   })
-              // );
             } else {
               handleError(res, err);
-            //   res.writeHead(404, headers);
-            //   res.write(
-            //     JSON.stringify({
-            //       status: "error",
-            //       message: "沒有找到該 id",
-            //     })
-            //   );
             }
-            // res.end();
           } catch(err) {
             handleError(res, err);
-            // res.writeHead(400, headers);
-            // res.write(
-            //   JSON.stringify({
-            //     status: "error",
-            //     message: "請求處理失敗",
-            //     error: err.message,
-            //   })
-            // );
-            // console.error(err);
-            // res.end();
           }
         });
     }else if(req.method == "OPTIONS"){
